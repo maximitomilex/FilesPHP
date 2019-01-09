aed83a0c3484f2a2ec08b72be41dea3fd0c7c8d2
Maximo Moreno <maximito1997@gmail.com>
2019-01-08 23:03:10 -0400

    Clase Conexion creada

---
+++ Models/Conexion.php
@@ -0,0 +1,27 @@
+<?php namespace Models;
+
+    class Conexion{
+
+        private $datos = array(
+          "host"=>"localhost",
+          "user"=>"root",
+          "pass"=>"",
+          "db"=>"Persona");
+        private $con;
+
+        public function __construct(){
+            $this->con= new\mysqli($datos["host"], $datos["user"], $datos["pass"],
+             $datos["db"]);
+        }
+
+        public function consultaSimple($sql){
+        	$this->con->query($sql);
+        }
+
+        public function consultaRetorno($sql){
+        	return $this->con->query($sql);
+        }
+
+    }
+
+?
