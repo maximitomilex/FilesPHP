c33774ec8491e979e34e0a15f1d48a8edc9c9300
Maximo Moreno <maximito1997@gmail.com>
2019-01-10 18:46:32 -0400

    Se actualizo el index.html

---
+++ Views/Persona/agregar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+   <head>
+   	   <title>Prueba</title>
+   </head>
+   <body>
+   	  <form action="" method="POST"enctype="multipart/form-data">
+   	  	 Nombre<br><input type="text" name="nombre">
+   	  	 apellido<br><input type="text" name="apellido">
+   	  	 dni<br><input type="text" name="dni">
+   	  	 FOTO<br><input type="file" name="imagen">
+   	  	 <input type="submit" value="Enviar">
+   	  </form>
+   </body>
+</html>
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
+define('DS',DIRECTORY_SEPARATOR);
+define('ROOT', realpath(dirname(__FILE__)) . DS);
+define('URL',"http://localhost/Files/");
 
 require_once "Config/Autoload.php";
 Config\Autoload::Run();
-  
-$persona = new Models\Persona();
-$dato=$persona->viewWhere("nombre","Junior");
-echo $dato["nombre"];
-
+require "Views/Persona/agregar.php";
+require_once "Controllers/personaController.php";
+$p= new Controllers\personaController();
+$p->agregar();
 ?>
